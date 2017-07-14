22ed6a605a998d4b32e8162df7e07a8e05724fbf
terehova <marinaterechova1974@gmail.com>
2017-07-14 17:12:41 +0000

    Add count function

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ def hello(name):
 def maximum(some_list)
      """Get maximum value from list"""
     return max(some_list)
-
+def count (some_list):
+    """Count elements in list"""
+    return len(some_list)
